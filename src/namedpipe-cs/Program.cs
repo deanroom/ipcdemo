acb9b0cbd03f0be75a718fac72adead4b82dc56c
JerryDing <deanroom@gmail.com>
2023-04-04 17:33:44 +0800

    chore:change runtime

--- src/namedpipe-cs/Program.cs
+++ src/namedpipe-cs/Program.cs
@@ -1,51 +1,50 @@
 ﻿using System;
-using System.IO;
-using System.IO.Pipes;
-using System.Net;
-using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32.SafeHandles;
 
 class Program
 {
+    // 声明 C 库函数
+    [DllImport("libc", EntryPoint = "mkfifo", SetLastError = true, CharSet = CharSet.Unicode)]
+    private static extern int mkfifo(string pathname, int mode);
+
+    [DllImport("libc", EntryPoint = "open", SetLastError = true, CharSet = CharSet.Unicode)]
+    private static extern SafeFileHandle open(string pathname, int flags);
+
+    [DllImport("libc", EntryPoint = "read", SetLastError = true)]
+    private static extern int read(int fd, byte[] buf, int count);
+
     static async Task Main(string[] args)
     {
-        // 创建 Unix 域套接字
-        Socket serverSocket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
-        EndPoint endPoint = new UnixDomainSocketEndPoint("/Users/jerryding/src/mypipe");
+        string fifoPath = "/tmp/myfifo3";
 
-        // 绑定到本地地址并监听连接请求
-        serverSocket.Bind(endPoint);
-        serverSocket.Listen(10);
+        if (!File.Exists(fifoPath))
+        {
+            int fifoMode = 0666; // 要创建的管道的权限
+            // 创建命名管道
+            int result = mkfifo(fifoPath, fifoMode);
 
-        Console.WriteLine("Server listening on Unix domain socket: /tmp/mysocket");
-
-        // 接受客户端连接并处理请求
+            if (result == 0)
+            {
+                Console.WriteLine("命名管道已成功创建！");
+            }
+            else
+            {
+                int error = Marshal.GetLastPInvokeError();
+                //var message = Marshal.GetLastPInvokeErrorMessage();
+                Console.WriteLine($"创建命名管道时发生错误，错误代码：{error},{message}");
+                return;
+            }
+        }
+        await using var stream = new FileStream(fifoPath,FileMode.Open,FileAccess.Read,FileShare.ReadWrite);
+        using var reader = new StreamReader(stream);
         while (true)
         {
-            Socket clientSocket = await serverSocket.AcceptAsync();
-
-            Console.WriteLine("Client connected.");
-
-            // 读取客户端发送的消息
-            byte[] buffer = new byte[1024];
-            int bytesRead = await clientSocket.ReceiveAsync(buffer, SocketFlags.None);
-
-            Console.WriteLine("Message received from client: {0}", Encoding.ASCII.GetString(buffer, 0, bytesRead));
-
-            // 向客户端发送响应
-            string response = "Hello from C# on Linux!";
-            byte[] responseBytes = Encoding.ASCII.GetBytes(response);
-            await clientSocket.SendAsync(responseBytes, SocketFlags.None);
-
-            Console.WriteLine("Response sent to client.");
-
-            // 关闭客户端套接字
-            clientSocket.Shutdown(SocketShutdown.Both);
-            clientSocket.Close();
+            Console.WriteLine(reader.ReadLine());
+            Thread.Sleep(1000);
         }
 
-        // 关闭服务端套接字
-        serverSocket.Close();
     }
 }
